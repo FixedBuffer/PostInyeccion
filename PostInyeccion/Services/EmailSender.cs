9de6f15fada69969ca95b4c7f1cf18120e8eee0d
Jorge Turrado Ferrero <jorge_turrado@hotmail.es>
2018-09-18 23:21:14 +0200

    Añado el EmailSender y su interfaz

---
+++ PostInyeccion/Services/EmailSender.cs
@@ -0,0 +1,14 @@
+﻿using PostInyeccion.Models;
+using System.Collections.Generic;
+
+namespace PostInyeccion.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public bool Enviar(List<Profesores> profesores)
+        {
+            //Enviar correo
+            return true;
+        }
+    }
+}
---
+++ PostInyeccion/Services/IEmailSender.cs
@@ -0,0 +1,10 @@
+﻿using PostInyeccion.Models;
+using System.Collections.Generic;
+
+namespace PostInyeccion.Services
+{
+    interface IEmailSender
+    {
+        bool Enviar(List<Profesores> profesores);
+    }
+}
