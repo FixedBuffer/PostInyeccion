023deced2f1a74cbf81d706ebf7400f8b8c9f2c6
Jorge Turrado Ferrero <jorge_turrado@hotmail.es>
2018-09-18 23:21:24 +0200

    Añado el GeneradorInformes.cs

---
+++ PostInyeccion/Services/GeneradorInformes.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PostInyeccion.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostInyeccion.Services
+{
+    internal class GeneradorInformes
+    {
+        //Dependencias Inyectadas
+        PostDbContext _contexto;
+        IEmailSender _emailSender;
+
+        //Inyectamos las dependencias en el constructor
+        internal GeneradorInformes(PostDbContext contexto, IEmailSender emailSender)
+        {
+            _contexto = contexto;
+            _emailSender = emailSender;
+        }
+
+        internal void GenerarInforme()
+        {
+            var profesores = _contexto.Profesores.Include(x => x.Cursos)
+                                                .ThenInclude(x => x.Alumnos).ToList();
+            //Trabajo de maquetacion
+            //.......
+
+            //Enviamos el correo
+            var envioOK = _emailSender.Enviar(profesores);
+            if (!envioOK)
+            {
+                //Registramos el fallo en un log 
+            }
+        }
+    }
+}
