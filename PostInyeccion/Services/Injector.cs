6dfdfed465237d7d46513563c521c8535b4218c2
Jorge Turrado Ferrero <jorge_turrado@hotmail.es>
2018-09-18 23:18:57 +0200

    Añado la clase Injector

---
+++ PostInyeccion/Services/Injector.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace PostInyeccion
+{
+    public static class Injector
+    {
+        static IServiceProvider _proveedor;
+
+        public static void GenerarProveedor(IServiceCollection serviceCollection)
+        {
+            _proveedor = serviceCollection.BuildServiceProvider();
+        }
+
+        public static T GetService<T>()
+        {
+            return _proveedor.GetService<T>();
+        }
+    }
+}
