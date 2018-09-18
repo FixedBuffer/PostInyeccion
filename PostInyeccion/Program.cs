54e78bdc38617beb1a56382da10a62082663fa82
Jorge Turrado Ferrero <jorge_turrado@hotmail.es>
2018-09-18 23:22:18 +0200

    Añado el codigo de Program.cs

---
+++ PostInyeccion/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PostInyeccion.Models;
+using PostInyeccion.Services;
+
+namespace PostInyeccion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Generamos la coleccion de servicios
+            IServiceCollection serviceCollection = new ServiceCollection();
+            //Registramos el DbContext
+            serviceCollection.AddDbContext<PostDbContext>(options=>
+                options.UseMySql("Server=localhost;Database=postefcore;Uid=root;Pwd=root;"));
+            //Registramos el EmailSender
+            serviceCollection.AddScoped<IEmailSender, EmailSender>();
+
+            Injector.GenerarProveedor(serviceCollection);
+
+            GeneradorInformes generador = new GeneradorInformes(Injector.GetService<PostDbContext>(), Injector.GetService<IEmailSender>());    
+            generador.GenerarInforme();
+        }
+    }   
+}
